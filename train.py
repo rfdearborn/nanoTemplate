20106656436292c207f11136a20f0f61141f58af
rfdearborn <rfdearborn@gmail.com>
2025-01-09 11:16:20 +0000

    Only save winners

--- train.py
+++ train.py
@@ -51,7 +51,7 @@ eval_interval = 1000
 log_interval = 10
 eval_iters = 200
 eval_only = False # if True, script exits right after the first eval
-always_save_checkpoint = True # if True, always save a checkpoint after each eval
+always_save_checkpoint = False # if True, always save a checkpoint after each eval
 init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
 # logging
 wandb_log = True
